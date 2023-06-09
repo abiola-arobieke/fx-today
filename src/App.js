8818f08f81c2daa10cf5c88ed4c3e8efc63f0c21
abiola <abiola.arobieke@gmail.com>
2023-06-09 13:19:51 -0400

    Add BrowserRouter, Routes, and route to App.js file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './container/Home';
+import ExchangeDetail from './components/homeComponents/ExchangeDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/currency/:id" element={<ExchangeDetail />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
