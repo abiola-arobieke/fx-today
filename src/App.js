b4e2b786e2541c3af66a9e1224519d0476438eca
abiola <abiola.arobieke@gmail.com>
2023-06-10 16:54:26 -0400

    Import Header.js and Footer.js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './container/Home';
+import Header from './components/headerFooter/Header';
 import ExchangeDetail from './components/homeComponents/ExchangeDetails';
+import Footer from './components/headerFooter/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/currency/:id" element={<ExchangeDetail />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
