638ee45d19a0e47a8d544663055742b12938b123
abiola <abiola.arobieke@gmail.com>
2023-06-10 16:49:55 -0400

    Modify ExchangeDetails.js and add css styling to it

--- src/components/homeComponents/ExchangeDetails.js
+++ src/components/homeComponents/ExchangeDetails.js
@@ -1,11 +1,12 @@
-import { useParams, Link } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSingleCurrency } from '../../redux/currency/currencySlice';
 import ExchangeDetailItem from './ExchangeDetailItem';
 
 const ExchangeDetails = () => {
   const { id } = useParams();
+  const [search, setSearch] = useState('');
   const dispatch = useDispatch();
   const apiUrls = [
     'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json',
@@ -21,13 +22,31 @@ const ExchangeDetails = () => {
   const selectedCurrency = myCurrency.singleCurrency;
 
   return (
-    <div>
-      <Link to="/">Back</Link>
-      <div>hello world </div>
-      <p>{id}</p>
-      {selectedCurrency?.map((currency) => (
-        <ExchangeDetailItem key={currency.code} currency={currency} />
-      ))}
+    <div className="container bg-screen txt-white">
+      <form className="d-flex search-form">
+        <input
+          id="searchBar"
+          type="search"
+          className="search"
+          placeholder="Search currency..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      <div className="d-flex f-end details">
+        <div className="md-6 x-title d-flex f-end">
+          <div>
+            <h2>{id}</h2>
+            <div>1.000</div>
+          </div>
+        </div>
+      </div>
+      <ul className="grid-details">
+        {selectedCurrency?.filter((currency) => (search.toLowerCase() === ''
+          ? currency
+          : currency.code.toLowerCase().includes(search))).map((currency) => (
+            <ExchangeDetailItem key={currency.code} currency={currency} />
+        ))}
+      </ul>
     </div>
   );
 };
