a4acb723d78ffcb2805ef6e54b9fafd63695d5c4
abiola <abiola.arobieke@gmail.com>
2023-06-17 04:01:18 -0400

    Modify ExhangeDetail.js file

--- src/components/homeComponents/ExchangeDetails.js
+++ src/components/homeComponents/ExchangeDetails.js
@@ -1,5 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSingleCurrency } from '../../redux/currency/currencySlice';
 import ExchangeDetailItem from './ExchangeDetailItem';
@@ -7,6 +9,7 @@ import ExchangeDetailItem from './ExchangeDetailItem';
 const ExchangeDetails = () => {
   const { id } = useParams();
   const [search, setSearch] = useState('');
+  const [amount, setAmount] = useState(1);
   const dispatch = useDispatch();
   const apiUrls = [
     'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json',
@@ -20,34 +23,61 @@ const ExchangeDetails = () => {
   const myCurrency = useSelector((store) => store.currencies);
   const selectedCurrency = myCurrency.singleCurrency;
 
+  const calculator = (e) => {
+    if (e.target.value === '') {
+      e.target.placeholder = 0.00;
+    }
+    setAmount(e.target.value);
+  };
+
   return (
     <div className="container bg-screen txt-white">
-      <form className="d-flex search-form">
-        <input
-          id="searchBar"
-          type="search"
-          className="search"
-          placeholder="Search currency..."
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </form>
-      <div className="d-flex info">
-        <div className="md-6 d-flex mid-center">
-          <img className="base-img" src={flag} alt="currency-flag" />
-        </div>
-        <div className="md-6 d-flex mid-center base-title">
-          <div className="md-6">
-            <h2>{id}</h2>
-            <div>1.000</div>
+      <div className="">
+        <form className="d-flex full-width mid-center">
+          <div className="d-flex m-10 search">
+            <FontAwesomeIcon className="s-center search-icon" icon={faMagnifyingGlass} />
+            <input
+              id="searchBar"
+              type="search"
+              className="searchBar"
+              placeholder="Search currency..."
+              onChange={(e) => setSearch(e.target.value)}
+            />
           </div>
+        </form>
+        <div className="d-flex banner">
+          <div className="md-6 d-flex bg-world">
+            {}
+          </div>
+          <div className="md-6 d-flex s-center base-title">
+            <div className="d-flex mid-center base-details">
+              <div>
+                <img className="base-img s-center" src={flag} alt="currency-flag" />
+              </div>
+              <div className="">
+                <h2>{id}</h2>
+                <input
+                  type="number"
+                  placeholder="1.00"
+                  className="amount"
+                  onChange={(e) => calculator(e)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="stat-title">
+          <span>Exchange rate for </span>
+          <span>{id}</span>
         </div>
       </div>
       <ul className="grid-details">
         {selectedCurrency?.filter((currency) => (search.toLowerCase() === ''
-          ? currency
-          : currency.code.toLowerCase().includes(search))).map((currency) => (
-            <ExchangeDetailItem key={currency.code} currency={currency} />
-        ))}
+          ? !(currency.code === `${id}`)
+          : currency.code.toLowerCase().includes(search)) && !(currency.code === `${id}`))
+          .map((currency) => (
+            <ExchangeDetailItem key={currency.code} currency={currency} amount={amount} />
+          ))}
       </ul>
     </div>
   );
